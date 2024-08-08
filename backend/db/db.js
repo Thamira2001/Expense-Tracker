9c8e8a2d04f27478b0f92aceeb62a878216aa815
Thamira Mishal Randeniya <130529871+Thamira2001@users.noreply.github.com>
2024-08-08 19:14:06 +0000

    fixed db connection issue by alllowing access from all ports

--- backend/db/db.js
+++ backend/db/db.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 // QY8H9DbTrRmCeDOS
 
 const db = async() => {
-    
+    //fixed the db connection issue by allowing access from all ports
     try{
         mongoose.set('strictQuery', false)
         await mongoose.connect(process.env.MONGO_URL)
