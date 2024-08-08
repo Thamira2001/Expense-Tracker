9dffaf235307fc03ad6b13beaadcdd12bd0f8966
Thamira Mishal Randeniya <130529871+Thamira2001@users.noreply.github.com>
2024-08-08 14:37:03 +0000

    first commit set up packages and set up running server and tested out with postman

---
+++ backend/app.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const cors = require('cors');
+const app = express()
+
+require('dotenv').config();
+
+const PORT = process.env.PORT;
+
+//middlewares
+
+app.use(express.json());
+app.use(cors({}))
+app.get('/', (req,res)=>{
+    res.send('Hello from PORT 5000')
+})
+
+const server = () => {
+    app.listen(PORT, () =>{
+        console.log('Listening to Port:', PORT)
+    })
+}
+
+server()
